53a1a49146201d8cd75660b5df06e196ba0c8016
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-02-16 23:12:04 +0530

    Add files via upload

--- multiplication table.js
+++ multiplication table.js
@@ -1,4 +1,19 @@
 let n = Number(prompt("enter a number"));
+if ( (n = 2) || (n = 9)){
 for (let i = 1; i <= 10; ++i) {
     document.write(n + " * " + i + " = " + n * i + "<br>");
+}
+
+}
+
+
+//this will accept numbers only between 2 to 9
+let n2;
+do {
+  n2 = prompt("Enter a number between 2 and 9");
+  n2 = parseInt(n2);
+} while (isNaN(n2) || n2 < 2 || n2 > 9);
+
+for (let i = 1; i <= 10; ++i) {
+  document.write(n2 + " * " + i + " = " + n2 * i + "<br>");
 }
