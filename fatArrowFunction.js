e35e20dc4f0114ab761caac0f50a4f9dd9d54e8f
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-02-20 23:51:23 +0530

    Create fatArrowFunction.js

---
+++ fatArrowFunction.js
@@ -0,0 +1,43 @@
+Complete the following program so that the square1() and square2() functions work properly.
+// Square the given number x
+function square1(x) {
+// TODO: complete the function code
+}
+// Square the given number x
+const square2 = x => // TODO: complete the function code
+
+
+//square1
+function square1(x){
+  return x * x;
+}
+square1(5);
+
+
+
+
+//square2
+const square2 = x => return x * x; // TODO: complete the function code
+
+
+
+//square1
+function square1(){
+  for(i=0;i<=10;i++){
+console.log(i * i);
+}
+square1();
+
+
+
+//square2
+const square2 =() => 
+ for(i=0;i<=10;i++){
+console.log(i
+ // TODO: complete the function code
+
+
+
+
+
+
