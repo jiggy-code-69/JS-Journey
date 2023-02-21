5a0e97a1eb76bb5dabbc45121caeaeda882800c5
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-02-21 21:54:54 +0530

    Update minmax.js

--- minmax.js
+++ minmax.js
@@ -19,3 +19,42 @@ min(44,5);
 
 
 
+// Calculator
+// Complete the following program so that it offers the four basic arithmetical operations: addition,
+// subtraction, multiplication and division. You can use either a function declaration or a function
+// expression.
+// // TODO: complete program
+// console.log(calculate(4, "+", 6)); // Must show 10
+// console.log(calculate(4, "-", 6)); // Must show -2
+// console.log(calculate(2, "*", 0)); // Must show 0
+// console.log(calculate(12, "/", 0)); // Must show Infinity
+
+const calculator = function calc(num1, operator, num2){
+ if(operator === '+'){
+     return num1 + num2;
+ }else if(operator === '-'){
+     return num1 - num2;
+ }else if(operator === '*'){
+     return num1 * num2;
+ }else if(operator === '/'){
+     return num1 / num2;
+ }else{
+     return Nan;
+ }
+    
+}
+console.log(calculate(4, "+", 6)); // Must show 10
+console.log(calculate(4, "-", 6)); // Must show -2
+console.log(calculate(2, "*", 0)); // Must show 0
+console.log(calculate(12, "/", 0)); // Must show Infinity
+
+
+
+
+
+
+
+
+
+
+
