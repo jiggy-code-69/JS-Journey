814322597a818a121fe6735e39dcc94d276ed0e9
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-02-24 21:37:23 +0530

    Update lexicalScoping.js

--- lexicalScoping.js
+++ lexicalScoping.js
@@ -13,8 +13,8 @@ outerFunction(); // Output: "Hello World"
 
 
 
-In this example, we have two nested functions: outerFunction and innerFunction. outerVariable is declared within outerFunction, while innerVariable is declared within innerFunction.
+// In this example, we have two nested functions: outerFunction and innerFunction. outerVariable is declared within outerFunction, while innerVariable is declared within innerFunction.
 
-When outerFunction is called, it declares the variable outerVariable and then calls innerFunction. When innerFunction is called, it declares the variable innerVariable and then logs the values of both outerVariable and innerVariable to the console.
+// When outerFunction is called, it declares the variable outerVariable and then calls innerFunction. When innerFunction is called, it declares the variable innerVariable and then logs the values of both outerVariable and innerVariable to the console.
 
-Because of lexical scoping, innerFunction has access to both outerVariable and innerVariable. However, outerFunction does not have access to innerVariable, since it is declared within innerFunction and is therefore scoped to that function only.
+// Because of lexical scoping, innerFunction has access to both outerVariable and innerVariable. However, outerFunction does not have access to innerVariable, since it is declared within innerFunction and is therefore scoped to that function only.
