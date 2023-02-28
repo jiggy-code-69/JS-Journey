c4ced880b3d2842873d7412954f5a3daf64485df
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-02-28 21:34:40 +0530

    Create hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+x = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element 
+elem.innerHTML = x;           // Display x in the element
+
+var x; // Declare x
+</script>
+
+</body>
+</html> 
