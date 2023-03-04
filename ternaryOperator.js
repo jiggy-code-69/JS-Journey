abd3b9f1f50bcd9b7d11bb870acbb11b4b0d9b5a
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-03-04 12:35:09 +0530

    Update ternaryOperator.js

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -1,8 +1,10 @@
 const age = 18;
 const isAdult = age >= 18 ? 'Yes' : 'No';
 console.log(isAdult); // Output: Yes
-The syntax of the ternary operator is:
 
-condition ? value_if_true : value_if_false;
 
-In this case, the condition is age >= 18, the value if true is 'Yes', and the value if false is 'No'. If the condition is true, the value if true is returned, otherwise the value if false is returned.
+// The syntax of the ternary operator is:
+
+// condition ? value_if_true : value_if_false;
+
+// In this case, the condition is age >= 18, the value if true is 'Yes', and the value if false is 'No'. If the condition is true, the value if true is returned, otherwise the value if false is returned.
