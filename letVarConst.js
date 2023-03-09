bf9e48b0474e18bf83850e7d0dc8adad8b2a4223
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-03-09 22:39:35 +0530

    Create letVarConst.js

---
+++ letVarConst.js
@@ -0,0 +1,14 @@
+// Using let
+let myVariable = 'Hello World';
+myVariable = 'Hi there'; // Valid - can reassign a new value to the variable
+console.log(myVariable); // Output: 'Hi there'
+
+// Using const
+const myConstant = 'Hello World';
+myConstant = 'Hi there'; // Invalid - cannot reassign a new value to a constant
+console.log(myConstant); // Output: 'Hello World'
+
+// Using var
+var myVar = 'Hello World';
+myVar = 'Hi there'; // Valid - can reassign a new value to the variable
+console.log(myVar); // Output: 'Hi there'
