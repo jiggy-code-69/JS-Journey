20227fa1413dd5db15dbaa471523991e18cef74a
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-03-30 22:05:53 +0530

    Update array.js

--- array.js
+++ array.js
@@ -23,3 +23,33 @@ let oneMovie = movies[0];
 let lengthOfArray = movies.length
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
 let lastIndexOfArray = movies[lengthOfArray - 1]
+
+
+
+
+
+
+
+
+
+
+//Create an array of movie titles. Loop through the array and each element to the h2.
+let  movies = ['Interstellar', 'Dumb and Dumber', 'Spiderman', 'IronMan'];
+for(let i = 0;i<movies.length;i++){
+    document.querySelector('h2').innerText += movies[i];
+   
+}
+
+
+//Create an array of numbers. Loop through the array and three to each number and replace the old number.
+let nums = [10,20,30];
+
+nums.forEach((a,b) => {
+nums[b]+= 3;
+});
+console.log(nums)
+//Find the average of all the numbers from question three
+let sum = 0;
+for (let i = 0; i<nums.length;i++){
+    sum += nums[i];
+}
