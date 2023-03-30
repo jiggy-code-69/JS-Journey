83e0237fce5f3500cd13f18fe05783cd395a5aa8
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-03-30 21:11:12 +0530

    Update array.js

--- array.js
+++ array.js
@@ -10,3 +10,16 @@ for ( let i = 0; i < rapperArray.length; i++){
   } 
 }
  console.log("And if you don't know, now you know.");
+
+
+
+
+
+//Create an array of movies with at least three movies.
+let movies = ['Interstellar', 'Inception', 'Notebook']
+//Using the array from above, store the first movie in a variable
+let oneMovie = movies[0];
+//Get the length of the original array and store it in a new variable
+let lengthOfArray = movies.length
+//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastIndexOfArray = movies[lengthOfArray - 1]
