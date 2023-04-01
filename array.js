4883f97b674bf376aae212f18571aa5c7da01353
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-04-01 12:52:49 +0530

    Update array.js

--- array.js
+++ array.js
@@ -53,3 +53,25 @@ let sum = 0;
 for (let i = 0; i<nums.length;i++){
     sum += nums[i];
 }
+
+
+// Write a program that creates the following array, then calculates and shows the array's max and  min value.
+
+// const values = [3, 11, 7, 2, 9, 10];
+
+const values = [3, 11, 7, 2, 9, 10];
+
+let max = values[0];
+let min = values[0];
+
+for (let i = 1; i < values.length; i++) {
+  if (values[i] > max) {
+    max = values[i];
+  }
+  if (values[i] < min) {
+    min = values[i];
+  }
+}
+
+console.log("The maximum value in the array is: " + max);
+console.log("The minimum value in the array is: " + min);
