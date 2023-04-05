1bc162cb9047bd08bb8b453295b8c85a2ad748bd
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-04-05 22:21:30 +0530

    Update array.js

--- array.js
+++ array.js
@@ -94,3 +94,18 @@ if(arr[0] < arr[arr.length-1]){
 
 }
 store([1,2,3]);
+
+
+
+
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiNumsInArr(arr){
+    let product = 1
+    arr.forEach( num => product *= num )
+    // for(let i = 0; i < arr.length; i++){
+    //     product *= arr[i]
+    // }
+
+    alert(product)
+}
+multiNumsInArr([10,2,3])
