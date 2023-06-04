6da3ece37e6c08105fdca80a9bfec05616398fb9
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-06-04 20:46:31 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -217,3 +217,29 @@ this.slide= function(){
 
 let Ji69Y = new streetFighterCharacter("Ji69Y","Aesthetic","Super_Powerfull","speed_of_light")
  
+//Arrays
+
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arr = [1,2,3,4,5]
+let sum = arr.reduce((a,y)=> a+y,0)
+console.log(sum)
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+function sqNum(arr){
+console.log( arr.map(x => x*2))
+}
+sqNum([1,2,3,4,5]);
+//Create a function that takes string
+//Print the reverse of that string to the console
+function reverseStrAndPalindrome(str){
+    let reverseStr =str.split("").reverse().join("")
+console.log(reverseStr)
+palindrome()
+}
+reverseStrAndPalindrome("12345")
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+function palindrome(){
+    reverseStr === str ? 'Palindrome' : 'Not Palindrome'
+}
+// palindrome()
