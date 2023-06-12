980798f9df237ac5a34437b5318a0752d48cdba0
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-06-12 20:44:20 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1037,3 +1037,361 @@ function divideArray(arr) {
       return [firstHalf, secondHalf];
     }
   }
+
+
+// ### Exercises: Level 1
+
+//   ```js
+//   const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
+
+//   const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB'
+//   ]
+
+//   const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+//   ```
+
+// 1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+for(let i = 0; i <= 10; i++){
+    console.log(`This is for loop ${i}`)
+}
+
+let  j=0
+while(j<=10){
+    console.log(`This is while loop ${j}`)
+    j++;
+}
+
+let k = 0
+do{
+    console.log(`This is do while loop ${k}`)
+    k++;
+}while(k <= 10)
+
+// 2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+for(let i = 10; i >= 0; i--){
+    console.log("!!!!!!!!!!!!!!!!!!!")
+    console.log(`This is for loop ${i}`)
+}
+
+let  l=10
+while(l>=0){
+    console.log("@@@@@@@@@@@@@@@@@@@@@@")
+    console.log(`This is while loop ${l}`)
+    l--;
+}
+
+let m = 10;
+do{
+    console.log("*************************")
+    console.log(`This is do while loop ${m}`);
+    m--;
+}while(m>=0)
+console.log("test")
+// 3. Iterate 0 to n using for loop
+// for(let i = 0; i<=n ; i++){
+//     console.log(i);
+// }
+// 4. Write a loop that makes the following pattern using console.log():
+
+//    ```js
+//        #
+//        ##
+//        ###
+//        ####
+//        #####
+//        ######
+//        #######
+//    ```
+for(let i = 7;i >= 1; i--){
+   let code = ""
+for(let j = 1;j <= i; j++){
+    code+="#"
+}
+console.log(code)
+}
+
+// 5. Use loop to print the following pattern:
+
+//    ```sh
+//    0 x 0 = 0
+//    1 x 1 = 1
+//    2 x 2 = 4
+//    3 x 3 = 9
+//    4 x 4 = 16
+//    5 x 5 = 25
+//    6 x 6 = 36
+//    7 x 7 = 49
+//    8 x 8 = 64
+//    9 x 9 = 81
+//    10 x 10 = 100
+//    ```
+
+for(let i = 0; i <= 10; i++){
+    console.log(`${i} x ${i} = ${i*i}`)
+}
+// 6. Using loop print the following pattern
+
+//    ```sh
+    // i    i^2   i^3
+    // 0    0     0
+    // 1    1     1
+    // 2    4     8
+    // 3    9     27
+    // 4    16    64
+    // 5    25    125
+    // 6    36    216
+    // 7    49    343
+    // 8    64    512
+    // 9    81    729
+    // 10   100   1000
+//    ```
+
+for(let i = 0; i <= 10; i++){
+    console.log(`${i}  ${i*i}  ${i**3}`)
+}
+// 7. Use for loop to iterate from 0 to 100 and print only even numbers
+// for(let i = 0; i <= 100; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+// 8. Use for loop to iterate from 0 to 100 and print only odd numbers
+// for(let i = 0; i <= 100; i++){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+// 9. Use for loop to iterate from 0 to 100 and print only prime numbers
+// for(let i = 2; i <= 100; i++){
+//     let isPrime = true;
+//     for(let j =2; j <i; j++){
+//         if( i % j === 0){
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if(isPrime){
+//     console.log(i)
+//     }
+// }
+// 10. Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+let sum = 0;
+for(let i = 0; i <= 100; i++){
+    sum+=i;
+    
+}
+console.log(sum)
+//     ```sh
+//     The sum of all numbers from 0 to 100 is 5050.
+//     ```
+
+// 11. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+
+let evenSum = 0;
+let oddSum = 0;
+
+for(let i = 0; i <= 100; i++){
+   if(i % 2 === 0){
+    evenSum+=i;
+   }else {
+    oddSum+=i;
+   }
+   
+}
+const arr =[evenSum, oddSum]
+console.log(arr);
+//     ```sh
+//     The sum of all evens from 0 to 100 is 2550. And the sum of all odds from 0 to 100 is 2500.
+//     ```
+
+// 12. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
+
+//     ```sh
+//       [2550, 2500]
+//     ```
+
+// 13. Develop a small script which generate array of 5 random numbers
+function randomNum(){
+    let randomNum = []
+    
+    for(i = 0; i <= 5;i++){
+      const random = Math.floor(Math.random() * 1000)
+      randomNum.push(random)
+    }
+    console.log(randomNum)
+}
+randomNum();
+
+// 14. Develop a small script which generate array of 5 random numbers and the numbers must be unique
+const uniqueRandomNumbers = [];
+
+while (uniqueRandomNumbers.length < 5) {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  if(!uniqueRandomNumbers.includes(randomNumber)){
+  uniqueRandomNumbers.push(randomNumber);
+  }
+}
+
+console.log("Unique Random Numbers:", Array.from(uniqueRandomNumbers));
+
+// 15. Develop a small script which generate a six characters random id:
+function randomChar(){
+    let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let randomId = "";
+    for(let i=0;i<6;i++){
+        const charIndex = Math.floor(Math.random()*characters.length)
+        randomId+=characters[charIndex]
+    }
+    console.log(randomId);
+}
+randomChar()
+//     ```sh
+//     5j2khz
+//     ```
+
+// ### Exercises: Level 2
+
+// 1. Develop a small script which generate any number of characters random id:
+
+//     ```sh
+//       fe3jo1gl124g
+//     ```
+function randomChar2(){
+    let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let randomId = "";
+    let random = Math.floor(Math.random()*100)
+    for(let i=0;i<random;i++){
+        const charIndex = Math.floor(Math.random()*characters.length)
+        randomId+=characters[charIndex]
+    }
+    console.log(randomId);
+}
+randomChar2()
+//     ```sh
+//       xkqci4utda1lmbelpkm03rba
+//     ```
+
+// 1. Write a script which generates a random hexadecimal number.
+
+//     ```sh
+//     '#ee33df'
+//     ```
+function hexaCode(){
+    let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let randomId = "";
+    // let random = Math.floor(Math.random()*100)
+    for(let i=0;i<6;i++){
+        const charIndex = Math.floor(Math.random()*characters.length)
+        randomId+=characters[charIndex]
+    }
+    console.log(`#${randomId}`);
+}
+hexaCode()
+// 1. Write a script which generates a random rgb color number.
+
+//     ```sh
+//     rgb(240,180,80)
+//     ```
+
+// 1. Using the above countries array, create the following new array.
+
+//     ```sh
+//     ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+//     ```
+
+// 1. Using the above countries array, create an array for countries length'.
+
+//     ```sh
+//     [7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]
+//     ```
+
+// 1. Use the countries array to create the following array of arrays:
+
+//     ```sh
+//       [
+//       ['Albania', 'ALB', 7],
+//       ['Bolivia', 'BOL', 7],
+//       ['Canada', 'CAN', 6],
+//       ['Denmark', 'DEN', 7],
+//       ['Ethiopia', 'ETH', 8],
+//       ['Finland', 'FIN', 7],
+//       ['Germany', 'GER', 7],
+//       ['Hungary', 'HUN', 7],
+//       ['Ireland', 'IRE', 7],
+//       ['Iceland', 'ICE', 7],
+//       ['Japan', 'JAP', 5],
+//       ['Kenya', 'KEN', 5]
+//     ]
+//     ```
+
+// 2. In above countries array, check if there is a country or countries containing the word 'land'. If there are countries containing 'land', print it as array. If there is no country containing the word 'land', print 'All these countries are without land'.
+
+//     ```sh
+//     ['Finland','Ireland', 'Iceland']
+//     ```
+
+// 3. In above countries array, check if there is  a country or countries end with a substring 'ia'. If there are countries end with, print it as array. If there is no country containing the word 'ai', print 'These are countries ends without ia'.
+
+//     ```sh
+//     ['Albania', 'Bolivia','Ethiopia']
+//     ```
+
+// 4. Using the above countries array, find the country containing the biggest number of characters.
+
+//       ```sh
+//       Ethiopia
+//       ```
+
+// 5. Using the above countries array, find the country containing only 5 characters.
+
+//     ```sh
+//     ['Japan', 'Kenya']
+//     ```
+
+// 6. Find the longest word in the webTechs array
+// 7. Use the webTechs array to create the following array of arrays:
+
+//     ```sh
+//     [["HTML", 4], ["CSS", 3],["JavaScript", 10],["React", 5],["Redux", 5],["Node", 4],["MongoDB", 7]]
+//     ```
+
+// 8. An application created using MongoDB, Express, React and Node is called a MERN stack app. Create the acronym MERN by using the array mernStack
+// 9. Iterate through the array, ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"] using a for loop or for of loop and print out the items.
+// 10. This is a fruit array , ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop without using a reverse method.
+// 11. Print all the elements of array as shown below.
+
+//     ```js
+//       const fullStack = [
+//         ['HTML', 'CSS', 'JS', 'React'],
+//         ['Node', 'Express', 'MongoDB']
+//       ]
+//     ````
+
+//     ```sh
+//       HTML
+//       CSS
+//       JS
+//       REACT
+//       NODE
+//       EXPRESS
+//       MONGODB
+//     
